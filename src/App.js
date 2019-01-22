93b703e6e9353f940e4fb8545c53d9b8a3c4e3fc
jade <mirar83@naver.com>
2019-01-22 23:22:28 +0900

    init

--- src/App.js
+++ src/App.js
@@ -3,10 +3,75 @@ import './App.css';
 import Movie from './Movie.js';
 
 class App extends Component {
+  // Render: componentWillMount() -> render() -> componentDidMount()
+  // Update: componentWillReceiveProps() -> shouldComponentUpdate() -> componentWillUpdate() -> render() -> componentDidUpdate()
+  
+  state = {}
+  
+  componentDidMount(){
+   this._getMovies()
+
+    /*setTimeout(() => {
+      this.setState({
+        movies: [
+          {
+            title : "If only",
+            poster : "http://image.cine21.com/resize/cine21/poster/2017/1121/9022_5a138ccf0e94d[X230,330].jpg" 
+          },
+          {
+            title : "Oldboy",
+            poster : "http://movie.phinf.naver.net/20111222_177/1324537084439rmrVk_JPEG/movie_image.jpg"
+          },
+          {
+            title : "Aquaman",
+            poster : "http://img.movist.com/?img=/x00/05/10/29_p1.jpg"
+          },
+          {
+            title : "Bohemian Rhapsody",
+            poster : "http://img.movist.com/?img=/x00/05/05/99_p1.jpg"
+          },
+          {
+            title: "Mamma mia",
+            poster: "https://images.shazam.com/coverart/t46105537-b284301095_s400.jpg"
+          }
+        ],
+          // ...this.state.movies,
+      })
+    },1000)*/
+  }
+
+  _renderMovies = () => {
+    const movies = this.state.movies.map(movie => {
+      console.log(movie)
+      return <Movie 
+      title={movie.title} 
+      poster={movie.medium_cover_image} 
+      key={movie.id}
+      genres={movie.genres}
+      synopsis={movie.synopsis}
+      />
+    })
+    return movies
+  } 
+
+  _getMovies = async () =>{
+    const movies = await this._callApi()
+    this.setState({
+      movies
+    })
+  }
+
+  _callApi = () =>{
+    return fetch('https://yts.am/api/v2/list_movies.json?sort_by=like_count')
+    .then(response => response.json())
+    .then(json => json.data.movies)
+    .catch(err => console.log('err'))
+  }
+
   render() {
     return (
       <div className="App">
-        <Movie />
+        {this.state.movies ? this._renderMovies() : 'Loading' }
       </div>
     );
   }
--- src/Movie.js
+++ src/Movie.js
@@ -1,23 +1,53 @@
-import React, { Component } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import './Movie.css';
 
-class Movie extends Component{
-    render(){
-        return(
-            <div>
-                <MoviePoster />
-                <h1>Hello this is a movie</h1>
+function Movie({title, poster, genres, synopsis}){
+    return(
+        <div className="Movie">
+            <div className="Movie__Columns">
+            <MoviePoster poster={poster} alt={title}/>
             </div>
-        )
-    }
+            <div className="Movie__Columns">
+                <h1>{title}</h1>
+                <div className="Movie__Genres">
+                {genres.map((genre, index) => <MovieGenre genre={genre} key={index}/>)}
+                </div>
+                <p className="Movie__synopsis">
+                {synopsis}
+                </p>
+            </div>
+        </div>
+    )
 }
 
-class MoviePoster extends Component{
-    render(){
-        return(
-            <img src="http://image.cine21.com/resize/cine21/poster/2017/1121/9022_5a138ccf0e94d[X230,330].jpg" />
-        )
-    }
+Movie.propTypes = {
+    title: PropTypes.string.isRequired,
+    poster: PropTypes.string.isRequired,
+    genres: PropTypes.array.isRequired,
+    synopsis: PropTypes.string.isRequired
+}
+
+
+
+function MovieGenre({genre}){
+    return(
+        <span className="Movie__Genre">{genre}</span>
+    )
+}
+
+function MoviePoster({poster, alt}){
+    return(
+        <img src={poster} alt={alt} title={alt} className="Movie__Poster" />
+    ) 
+}
+
+MovieGenre.prototype = {
+    genre: PropTypes.string.isRequired
+}
+
+MoviePoster.propTypes = {
+    poster: PropTypes.string.isRequired
 }
 
 export default Movie;
