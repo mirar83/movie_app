67d5910192ffe551c7c5795ca128f74093579776
jade <mirar83@naver.com>
2019-01-26 02:36:16 +0900

    Updates

---
+++ precache-manifest.2301ce56b00b0c104d8c7eb556ae6255.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f9521ac5b62e87334b5b",
+    "url": "/movie_app/static/js/runtime~main.f9521ac5.js"
+  },
+  {
+    "revision": "faca1c5b1c72657bd9d6",
+    "url": "/movie_app/static/js/main.faca1c5b.chunk.js"
+  },
+  {
+    "revision": "be9ff9f0d6b66aea30bf",
+    "url": "/movie_app/static/js/1.be9ff9f0.chunk.js"
+  },
+  {
+    "revision": "faca1c5b1c72657bd9d6",
+    "url": "/movie_app/static/css/main.4658ae74.chunk.css"
+  },
+  {
+    "revision": "c8d02ffdb6e53641d71bf2c8e5d2d193",
+    "url": "/movie_app/index.html"
+  }
+];
